9356de87163f8d244b6a047ee3d5b56191f6f39a
Jeroen Vennegoor op Nijhuis <jeroen.vennegoor@gmail.com>
2021-06-26 15:40:07 +0200

    some more changes

--- src/server.cpp
+++ src/server.cpp
@@ -19,18 +19,21 @@
 #include <iostream>
 #include <memory>
 #include <string>
-#include <stdio.h>
-#include <string.h>
-
+//#include <string>
+#include <sstream>
+#include <vector>
+#include <iterator>
 
 #include <grpcpp/ext/proto_server_reflection_plugin.h>
 #include <grpcpp/grpcpp.h>
 #include <grpcpp/health_check_service_interface.h>
+#include <google/protobuf/dynamic_message.h>
 #include "rocksdb/db.h"
 #include "rocksdb/slice.h"
 #include "rocksdb/options.h"
 #include "propanedb.grpc.pb.h"
 
+using google::protobuf::Any;
 using grpc::Server;
 using grpc::ServerBuilder;
 using grpc::ServerContext;
@@ -39,8 +42,6 @@ using propane::Database;
 using propane::PropaneEntity;
 using propane::PropaneId;
 using propane::PropaneStatus;
-using google::protobuf::Any;
-
 
 using namespace ROCKSDB_NAMESPACE;
 using namespace std;
@@ -51,33 +52,96 @@ std::string kDBPath = "C:\\Windows\\TEMP\\rocksdb_simple_example";
 std::string kDBPath = "/tmp/rocksdb_simple_example";
 #endif
 
-class DatabaseServiceImpl final : public Database::Service {
+class DatabaseServiceImpl final : public Database::Service
+{
 
-  // DatabaseServiceImpl(rocksdb::DB db){
-  //   this.db = db;
+public:
+  DatabaseServiceImpl()
+  {
 
-  // }
+    Options options;
+    // Optimize RocksDB. This is the easiest way to get RocksDB to perform well
+    options.IncreaseParallelism();
+    options.OptimizeLevelStyleCompaction();
+    // create the DB if it's not already present
+    options.create_if_missing = true;
+    // open DB
+    ROCKSDB_NAMESPACE::Status s = DB::Open(options, kDBPath, &db);
+    assert(s.ok());
+  }
 
-  grpc::Status Put(ServerContext* context, const PropaneEntity* request,
-                  PropaneStatus* reply) override {
+  ~DatabaseServiceImpl()
+  {
+
+    db->Close();
+    delete db;
+  }
+
+  template <typename Out>
+  void split(const std::string &s, char delim, Out result)
+  {
+    std::istringstream iss(s);
+    std::string item;
+    while (std::getline(iss, item, delim))
+    {
+      *result++ = item;
+    }
+  }
+
+  std::vector<std::string> split(const std::string &s, char delim)
+  {
+    std::vector<std::string> elems;
+    split(s, delim, std::back_inserter(elems));
+    return elems;
+  }
+
+  grpc::Status Put(ServerContext *context, const PropaneEntity *request,
+                   PropaneStatus *reply) override
+  {
     //std::string prefix("Hello ");
     //reply->set_message(prefix + request->name());
-    Any any= request->data();
+    google::protobuf::DynamicMessageFactory dmf;
+    //google::protobuf::DescriptorPool pool;
+    const google::protobuf::DescriptorPool *pool = google::protobuf::DescriptorPool::generated_pool();
+    //pool->
+
+    Any any = request->data();
     string typeUrl = any.type_url();
-    cout << "TypeURL=" <<typeUrl<< endl;
+    cout << "Any TypeURL=" << typeUrl << endl;
+    string typeName = split(typeUrl, '/')[1];
+    cout << "Any TypeName=" << typeName << endl;
+
+    const google::protobuf::Descriptor *descriptor = pool->FindMessageTypeByName(typeName);
+    if (descriptor != nullptr)
+    {
+      cout << "Descriptor=" << descriptor->DebugString() << endl;
+      google::protobuf::Message *message = dmf.GetPrototype(descriptor)->New();
+      any.UnpackTo(message);
+      cout << "Message Debug String=" << message->DebugString() << endl;
+
+      const google::protobuf::FieldDescriptor *fd = descriptor->FindFieldByName("id");
+      const google::protobuf::Reflection *reflection = message->GetReflection();
+      string id = reflection->GetString(*message, fd);
+      cout << "Message ID= " << id << endl;
+
+      string serializedAny ;
+      any.SerializeToString(&serializedAny);
+
+      ROCKSDB_NAMESPACE::Status s = db->Put(WriteOptions(), id,  serializedAny);
+      assert(s.ok());
+    }
 
     propane::TestEntity test;
-    typeUrl = test.GetTypeName();
-    cout << "Type name=" <<typeUrl<< endl;
+    typeName = test.GetTypeName();
+    cout << "Type name=" << typeName << endl;
     //string clazzName =strtok(typeUrl,"/")[1]
-        //string clazzName = typeUrl.split("/")[1];
-        //string clazzPackage = sprintf("package.%s", clazzName);
-        //Class<Message> clazz = (Class<Message>) Class.forName(clazzPackage);
-        //return childMessage.unpack(clazz);
-        //any.UnpackTo(message);
+    //string clazzName = typeUrl.split("/")[1];
+    //string clazzPackage = sprintf("package.%s", clazzName);
+    //Class<Message> clazz = (Class<Message>) Class.forName(clazzPackage);
+    //return childMessage.unpack(clazz);
+    //any.UnpackTo(message);
     //String clazzName = any->getTypeUrl().split("/")[1];
     //any->PackFrom(entity);
-
 
     //ROCKSDB_NAMESPACE::Status s = db->Put(WriteOptions(), request->data(), "value");
     //assert(s.ok());
@@ -85,24 +149,16 @@ class DatabaseServiceImpl final : public Database::Service {
     reply->set_statusmessage("OK");
     return grpc::Status::OK;
   }
+
+private:
+  DB *db;
 };
 
-void RunServer() {
+void RunServer()
+{
   std::string server_address("0.0.0.0:50051");
-  DB* db;
+
   DatabaseServiceImpl service;
-
-  Options options;
-  // Optimize RocksDB. This is the easiest way to get RocksDB to perform well
-  options.IncreaseParallelism();
-  options.OptimizeLevelStyleCompaction();
-  // create the DB if it's not already present
-  options.create_if_missing = true;
-   // open DB
-  ROCKSDB_NAMESPACE::Status s = DB::Open(options, kDBPath, &db);
-  assert(s.ok());
-
-
 
   grpc::EnableDefaultHealthCheckService(true);
   grpc::reflection::InitProtoReflectionServerBuilderPlugin();
@@ -121,7 +177,8 @@ void RunServer() {
   server->Wait();
 }
 
-int main(int argc, char** argv) {
+int main(int argc, char **argv)
+{
   RunServer();
 
   return 0;
