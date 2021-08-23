325bcc12c91d00f6fe1b7b91a6315251546c4871
Jeroen Vennegoor op Nijhuis <jeroen.vennegoor@gmail.com>
2021-08-23 21:46:34 +0200

    Fix dockerfile

--- scripts/buildDockerProd.sh
+++ scripts/buildDockerProd.sh
@@ -1,2 +1,1 @@
-docker build -t propanedb-prod1 -f ./docker-prod1/Dockerfile .
-docker build -t jevon82/propanedb -f ./docker-prod2/Dockerfile .
+docker build -t jevon82/propanedb -f ./docker-prod/Dockerfile .
