191adb22b02aa1e1ae58740de0de771f6639982e
Jeroen Vennegoor op Nijhuis <jeroen.vennegoor@gmail.com>
2022-02-01 17:05:38 +0100

    update uploadCoverage script

--- scripts/uploadCoverage.sh
+++ scripts/uploadCoverage.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 gcovr -x test_coverage.xml
-bash <(curl -Ls https://coverage.codacy.com/get.sh) report -r test_coverage.xml
+bash <(curl -Ls https://coverage.codacy.com/get.sh) report -l CPP --force-language  -r test_coverage.xml
